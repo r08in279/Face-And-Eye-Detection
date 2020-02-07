c2e2bbfcb428304073631c2ac169cc9e2bc99ebf
r08in279 <54441078+r08in279@users.noreply.github.com>
2020-02-07 14:42:32 +0530

    Add files via upload

---
+++ hand_detection.py
@@ -0,0 +1,24 @@
+import cv2
+
+hand_cascade = cv2.CascadeClassifier('hand.xml') # We load the cascade for the hand
+
+def detect(gray, frame): # We create a function that takes as input the image in black and white (gray) and the original image (frame), and that will return the same image with the detector rectangles. 
+    faces = hand_cascade.detectMultiScale(gray, 1.3, 5) # We apply the detectMultiScale method from the face cascade to locate one or several faces in the image.
+    for (x, y, w, h) in faces: # For each detected face:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) # We paint a rectangle around the face.
+        roi_gray = gray[y:y+h, x:x+w] # We get the region of interest in the black and white image.
+        roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.
+    return frame # We return the image with the detector rectangles.
+
+video_capture = cv2.VideoCapture(1) # We turn the webcam on.
+
+while True: # We repeat infinitely (until break):
+    _, frame = video_capture.read() # We get the last frame.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # We do some colour transformations.
+    canvas = detect(gray, frame) # We get the output of our detect function.
+    cv2.imshow('Video', canvas) # We display the outputs.
+    if cv2.waitKey(1) & 0xFF == ord('q'): # If we type on the keyboard:
+        break # We stop the loop.
+
+video_capture.release() # We turn the webcam off.
+cv2.destroyAllWindows() # We destroy all the windows inside which the images were displayed.
